3a130af4cbd25a312ef15b5a3986aed33ae07ef9
dmeurisse <info@mchobby.be>
2020-08-13 11:27:04 +0200

    add Vera in size 8,9

--- generate-all-fonts.sh
+++ generate-all-fonts.sh
@@ -16,18 +16,26 @@ python3 ft-generate.py ttf-fonts/Entypo.otf 42 upy-fonts/etypo_23.py Entypo_23 -
 python3 ft-generate.py ttf-fonts/Arrows.ttf 24 upy-fonts/arrows_15.py Arrows_15 --descenders= --special-align=#65:M,#66:M,#73:M,#74:M,#81:M,#82:M,#83:M,#84:M,#85:M,#86:M,#87:M,#88:M,#97:M,#98:M --chars=#65-#90,#97-#122
 python3 ft-generate.py ttf-fonts/Arrows.ttf 36 upy-fonts/arrows_23.py Arrows_23 --descenders= --special-align=#65:M,#66:M,#73:M,#74:M,#81:M,#82:M,#83:M,#84:M,#85:M,#86:M,#87:M,#88:M,#97:M,#98:M --chars=#65-#90,#97-#122
 
+python3 ft-generate.py ttf-fonts/Vera.ttf 8 upy-fonts/vera_8.py Vera_8 --chars=$FULL_CHARSET
+python3 ft-generate.py ttf-fonts/Vera.ttf 9 upy-fonts/vera_9.py Vera_9 --chars=$FULL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 10 upy-fonts/vera_10.py Vera_10 --chars=$FULL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 14 upy-fonts/vera_15.py Vera_15 --chars=$FULL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 23 upy-fonts/vera_23.py Vera_23 --chars=$FULL_CHARSET
 
+python3 ft-generate.py ttf-fonts/Vera.ttf 8 upy-fonts/vera_m8.py Vera_m8 --chars=$MINIMAL_CHARSET
+python3 ft-generate.py ttf-fonts/Vera.ttf 9 upy-fonts/vera_m9.py Vera_m9 --chars=$MINIMAL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 10 upy-fonts/vera_m10.py Vera_m10 --chars=$MINIMAL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 14 upy-fonts/vera_m15.py Vera_m15 --chars=$MINIMAL_CHARSET
 python3 ft-generate.py ttf-fonts/Vera.ttf 23 upy-fonts/vera_m23.py Vera_m23 --chars=$MINIMAL_CHARSET
 
-python3 ft-generate.py ttf-fonts/VeraMono.ttf 14 upy-fonts/veram_15.py VeraMono_15 --chars=$FULL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 9 upy-fonts/veram_9.py VeraMono_9 --chars=$FULL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 10 upy-fonts/veram_10.py VeraMono_10 --chars=$FULL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 15 upy-fonts/veram_15.py VeraMono_15 --chars=$FULL_CHARSET
 python3 ft-generate.py ttf-fonts/VeraMono.ttf 23 upy-fonts/veram_23.py VeraMono_23 --chars=$FULL_CHARSET
 
-python3 ft-generate.py ttf-fonts/VeraMono.ttf 14 upy-fonts/veram_m15.py VeraMono_m15 --chars=$MINIMAL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 9 upy-fonts/veram_m9.py VeraMono_m9 --chars=$MINIMAL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 10 upy-fonts/veram_m10.py VeraMono_m10 --chars=$MINIMAL_CHARSET
+python3 ft-generate.py ttf-fonts/VeraMono.ttf 15 upy-fonts/veram_m15.py VeraMono_m15 --chars=$MINIMAL_CHARSET
 python3 ft-generate.py ttf-fonts/VeraMono.ttf 23 upy-fonts/veram_m23.py VeraMono_m23 --chars=$MINIMAL_CHARSET
 
 python3 ft-generate.py ttf-fonts/PitchDisplayRegularDemo.ttf 14 upy-fonts/pitch_15.py Pitch_15 --chars=$FULL_CHARSET
