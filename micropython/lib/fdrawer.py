fee1272900c89a2606ab7b19baccbd23f268e80a
dmeurisse <info@mchobby.be>
2020-12-13 01:01:50 +0100

    Space (#32) drawing. Use memoryView, add property space & spacing

--- micropython/lib/fdrawer.py
+++ micropython/lib/fdrawer.py
@@ -23,7 +23,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import os
 
@@ -43,11 +43,15 @@ class FontLoader:
 			raise Exception( "Invalid Magic Key!" )
 
 		# Read information
-		self.width  = f.read(1)[0]
+		self.width  = f.read(1)[0] # Typical width
 		self.height = f.read(1)[0]
 		self.datasize = f.read(1)[0]
 		self.entries  = f.read(1)[0]
 		self.data = f.read() # read the remain of the file
+		self.mv = memoryview(self.data)
+
+	def get_width( self, word ):
+		return len(word)*self.width
 
 	def _extract_char_data( self, cursor, count ):
 		# Extract the character data from the cursor position (composed of count items)
@@ -55,7 +59,7 @@ class FontLoader:
 		for i in range(count):
 			_d = 0
 			for _datasize in range(self.datasize):
-				_d += self.data[cursor]
+				_d += self.mv[cursor] # self.data[cursor]
 				if _datasize < (self.datasize-1):
 					_d = _d << 8
 				cursor += 1
@@ -67,8 +71,8 @@ class FontLoader:
 		    Will raise an exception if character is not available in the list """
 		assert 0 <= char_code <= 255
 		_cursor = 0
-		_ch  = self.data[_cursor]
-		_size = self.data[_cursor+1]
+		_ch  = self.mv[_cursor] #self.data[_cursor]
+		_size = self.mv[_cursor+1] #self.data[_cursor+1]
 		while _ch != None:
 			if _ch==char_code:
 				# Char char_code found!
@@ -79,8 +83,8 @@ class FontLoader:
 				if _cursor >= len(self.data):
 					_ch = None
 				else:
-					_ch = self.data[_cursor]
-					_size = self.data[_cursor+1]
+					_ch = self.mv[_cursor] #self.data[_cursor]
+					_size = self.mv[_cursor+1] #self.data[_cursor+1]
 		raise KeyError( "Missing char_code %i in font data" % char_code )
 
 
@@ -92,6 +96,12 @@ class FontDrawer:
 		self._fontcolor = (1) # Font color
 		self._bgcolor   = (0) # Background color
 		self._font = FontLoader( font_name )
+		self._space   = len(self._font[73]) # space char width = idem to capital I width
+		self._spacing = 2 #  space between chars
+
+	@property
+	def font( self ):
+		return self._font
 
 	@property
 	def color( self ):
@@ -113,6 +123,16 @@ class FontDrawer:
 		""" FrameBuffer color value used to drawing background.
 		 	(TODO) None will use the pixel background color for the drawed pixel """
 		self._bgcolor = value
+
+	@property
+	def spacing( self ):
+		""" Additional space between characters (in pixels) """
+		return self._spacing
+
+	@spacing.setter
+	def spacing( self, value ):
+		assert 0 <= spacing <= 10
+		self._spacing = value
 
 	@property
 	def scale( self ):
@@ -153,6 +173,9 @@ class FontDrawer:
 			assert char <= 255
 			index = char
 
+		if index==32: # space
+			return self._space*self._fontscale, self._space*self._fontscale
+
 		try:
 			chrwidth = len(self._font[index]) * self._fontscale
 			data = self._font[index]
@@ -174,4 +197,4 @@ class FontDrawer:
 		for ch in text:
 			widths = self.print_char( ch, xpos, y )
 			xpos += widths[0] # add proportional_witdth of characters
-			xpos += 2*self._fontscale # 2 pixels spacing between chars
+			xpos += self._spacing*self._fontscale # 2 pixels spacing between chars
